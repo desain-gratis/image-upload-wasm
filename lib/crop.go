e6f2f70fb37b278c7a97a7be03b8114555205ab0
keenan.gebze <keenan.gebze@gmail.com>
2022-11-23 04:43:37 +0700

    improvements. lots of them!
     So proud for the new 'CropByCenterAndSpoke' algorithm

--- lib/crop.go
+++ lib/crop.go
@@ -5,44 +5,61 @@ import (
 	"image"
 )
 
-func CropByCenterAndSpoke(rect image.Rectangle, center image.Point, ratioX int, ratioY, spoke int) image.Rectangle {
+func CropByCenterAndSpoke(rect image.Rectangle, centerX int, centerY int, ratioX int, ratioY, spoke int) image.Rectangle {
 
 	// We just need to find the max width here or use spoke
-	x1 := center.X
-	x2 := rect.Max.X - center.X
-	y1 := center.Y
-	y2 := rect.Max.Y - center.Y
+	x1 := centerX
+	x2 := rect.Max.X - centerX
+	y1 := centerY
+	y2 := rect.Max.Y - centerY
 
 	minX := x1
 	if x2 < x1 {
-		minX = x1
+		minX = x2
 	}
 	minY := y1
 	if y2 < y1 {
 		minY = y2
 	}
 
-	// if bounded by width, we don't do anything
-	byHeight := false
-	size := minX * 2
-	if minY < minX {
-		byHeight = true
-		size = minY * 2
+	// how many height can be taken, given limited width (min width)
+	// how many width can be taken, given limited height (min height)
+	maxWidth := minY * ratioX / ratioY
+	maxHeight := minX * ratioY / ratioX
+
+	// Check between the two, whos limiting who
+	boundedByWidth := false
+	if maxWidth < maxHeight {
+		boundedByWidth = true
 	}
 
-	if spoke < size {
-		size = spoke
+	width := maxHeight * ratioX / ratioY
+	height := maxHeight
+
+	if boundedByWidth {
+		width = maxWidth
+		height = maxWidth * ratioY / ratioX
 	}
 
-	return cropFromPointByWidth(ratioX, ratioY, size, byHeight, center)
+	return cropByCenterWidthHeight(centerX, centerY, width*2, height*2)
+	// return cropFromPointByWidth(ratioX, ratioY, size, byHeight, centerX, centerY)
+}
+
+func cropByCenterWidthHeight(centerX int, centerY int, width int, height int) image.Rectangle {
+	_minx := float64(centerX) - float64(width)/2
+	_miny := float64(centerY) - float64(height)/2
+	_maxx := float64(centerX) + float64(width)/2
+	_maxy := float64(centerY) + float64(height)/2
+
+	return image.Rect(int(_minx), int(_miny), int(_maxx), int(_maxy))
 }
 
 // resizeFromPoint
-func cropFromPointByWidth(ratioX, ratioY int, size int, byHeight bool, center image.Point) image.Rectangle {
+func cropFromPointByWidth(ratioX, ratioY int, size int, byHeight bool, centerX int, centerY int) image.Rectangle {
 	if size <= 0 || ratioX <= 0 || ratioY <= 0 {
 		return image.Rectangle{
-			Min: center,
-			Max: center,
+			Min: image.Pt(centerX, centerY),
+			Max: image.Pt(centerX, centerY),
 		}
 	}
 
@@ -55,10 +72,10 @@ func cropFromPointByWidth(ratioX, ratioY int, size int, byHeight bool, center im
 		uH = float64(1)
 	}
 
-	_minx := float64(center.X) - float64(size)*uW/2
-	_miny := float64(center.Y) - float64(size)*uH/2
-	_maxx := float64(center.X) + float64(size)*uW/2
-	_maxy := float64(center.Y) + float64(size)*uH/2
+	_minx := float64(centerX) - float64(size)*uW/2
+	_miny := float64(centerY) - float64(size)*uH/2
+	_maxx := float64(centerX) + float64(size)*uW/2
+	_maxy := float64(centerY) + float64(size)*uH/2
 
 	return image.Rect(int(_minx), int(_miny), int(_maxx), int(_maxy))
 }
--- main.go
+++ main.go
@@ -18,14 +18,74 @@ import (
 
 func main() {
 	js.Global().Set("GetSize", GetSize())
-	js.Global().Set("CenterCrop", CenterCrop())
+	js.Global().Set("Crop", Crop())
+	js.Global().Set("GetRGBA", GetRGBA())
+	js.Global().Set("CropRGBA", CropRGBA())
 	select {}
 }
 
-func GetSize() js.Func {
+func CropRGBA() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) < 4 {
+			fmt.Println("Ohnono, args < 5")
+			return nil
+		}
+
+		size := args[0].Int()
+		data := args[1]
+		width := args[2].Int()
+		height := args[3].Int()
+		stride := args[4].Int()
+
+		centerX := args[5].Int()
+		centerY := args[6].Int()
+		ratioX := args[7].Int()
+		ratioY := args[8].Int()
+		spoke := args[9].Int()
+
+		if size > (100 * (1 << 20)) {
+			fmt.Println("Ohnono, size too large (greater than 100 Mb)")
+			return nil
+		}
+
+		buffer := make([]byte, size)
+		bytesRead := js.CopyBytesToGo(buffer, data)
+		// in case we use fixed sized buffer
+		buffer = buffer[:bytesRead]
+
+		originalCopy := image.NewRGBA(image.Rect(0, 0, width, height))
+		originalCopy.Pix = buffer
+		originalCopy.Stride = stride
+
+		crop := lib.CropByCenterAndSpoke(originalCopy.Bounds(), centerX, centerY, ratioX, ratioY, spoke)
+
+		fmt.Println("DEBUG", crop.Dx(), crop.Dy())
+		// fmt.Println(crop.Min.X, crop.Min.Y, crop.Max.X, crop.Max.Y)
+		// fmt.Println(crop)
+
+		// todo can use sub image
+		cropped := image.NewRGBA(image.Rect(0, 0, crop.Max.X-crop.Min.X, crop.Max.Y-crop.Min.Y))
+		draw.Draw(cropped, cropped.Rect, originalCopy, image.Pt(0, 0), draw.Src)
+
+		dst := js.Global().Get("Uint8Array").New(len(cropped.Pix))
+		_ = js.CopyBytesToJS(dst, cropped.Pix)
+
+		return map[string]interface{}{
+			"success": map[string]interface{}{
+				"data":   dst,
+				"width":  cropped.Rect.Dx(),
+				"height": cropped.Rect.Dy(),
+				"stride": cropped.Stride,
+			},
+		}
+	})
+}
+
+// GetRGBA without using Canvas :(
+func GetRGBA() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) < 2 {
-			fmt.Println("Ohnono, args <= 0")
+			fmt.Println("Ohnono, args < 2")
 			return nil
 		}
 
@@ -33,14 +93,65 @@ func GetSize() js.Func {
 		data := args[1]
 
 		if size > (10 * (1 << 20)) {
-			fmt.Println("Ohnono, size too large")
+			fmt.Println("Ohnono, size too large (greater than 10 Mb)")
 			return nil
 		}
 
 		buffer := make([]byte, size)
-
 		bytesRead := js.CopyBytesToGo(buffer, data)
+		// in case we use fixed sized buffer
+		buffer = buffer[:bytesRead]
 
+		r := bytes.NewReader(buffer)
+		// imc, format, err := image.DecodeConfig(r)
+		// fmt.Printf("Size: %+v x %+v; format: %v, err: %v\n", imc.Width, imc.Height, format, err)
+		// fmt.Printf("Format: %v\n", format)
+		// fmt.Printf("Err: %v\n", err)
+		// r.Reset(buffer)
+
+		img, _, err := image.Decode(r)
+		if err != nil {
+			return map[string]interface{}{
+				"failed": err.Error(),
+			}
+		}
+
+		originalCopy := image.NewRGBA(image.Rect(0, 0, img.Bounds().Dx(), img.Bounds().Dy()))
+
+		draw.Draw(originalCopy, originalCopy.Rect, img, image.Pt(0, 0), draw.Src)
+
+		dst := js.Global().Get("Uint8Array").New(len(originalCopy.Pix))
+		_ = js.CopyBytesToJS(dst, originalCopy.Pix)
+
+		return map[string]interface{}{
+			"success": map[string]interface{}{
+				"data":   dst,
+				"width":  originalCopy.Rect.Dx(),
+				"height": originalCopy.Rect.Dy(),
+				"stride": originalCopy.Stride,
+			},
+		}
+
+	})
+}
+
+func GetSize() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) < 2 {
+			fmt.Println("Ohnono, args < 2")
+			return nil
+		}
+
+		size := args[0].Int()
+		data := args[1]
+
+		if size > (10 * (1 << 20)) {
+			fmt.Println("Ohnono, size too large (greater than 10 Mb)")
+			return nil
+		}
+
+		buffer := make([]byte, size)
+		bytesRead := js.CopyBytesToGo(buffer, data)
 		// in case we use fixed sized buffer
 		buffer = buffer[:bytesRead]
 
@@ -55,12 +166,13 @@ func GetSize() js.Func {
 	})
 }
 
-// CenterCrop returns a JavaScript function
-func CenterCrop() js.Func {
+// Crop returns a JavaScript function
+func Crop() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		if len(args) < 2 {
-			fmt.Println("Ohnono, args <= 0")
-			return nil
+			return map[string]interface{}{
+				"failed": "Ohnono, args <= 0",
+			}
 		}
 
 		size := args[0].Int()
@@ -74,8 +186,9 @@ func CenterCrop() js.Func {
 		// validate size too large
 
 		if size > (10 * (1 << 20)) {
-			fmt.Println("Ohnono, size too large")
-			return nil
+			return map[string]interface{}{
+				"failed": "Ohnono, size too large",
+			}
 		}
 
 		buffer := make([]byte, size)
@@ -86,53 +199,41 @@ func CenterCrop() js.Func {
 		buffer = buffer[:bytesRead]
 
 		r := bytes.NewReader(buffer)
-		imc, format, err := image.DecodeConfig(r)
-		fmt.Printf("Size: %+v x %+v; format: %v, err: %v\n", imc.Width, imc.Height, format, err)
-		fmt.Printf("Format: %v\n", format)
-		fmt.Printf("Err: %v\n", err)
+		// imc, format, err := image.DecodeConfig(r)
+		// fmt.Printf("Size: %+v x %+v; format: %v, err: %v\n", imc.Width, imc.Height, format, err)
+		// fmt.Printf("Format: %v\n", format)
+		// fmt.Printf("Err: %v\n", err)
+		// r.Reset(buffer)
 
-		r.Reset(buffer)
-		img, format, err := image.Decode(r)
+		img, _, err := image.Decode(r)
+		if err != nil {
+			return map[string]interface{}{
+				"failed": err.Error(),
+			}
+		}
 
 		// fmt.Println("%+v\n", img.)
-		fmt.Printf("Format is: %v\n", format)
-		fmt.Printf("Err: %v\n", err)
+		// fmt.Printf("Format is: %v\n", format)
+		// fmt.Printf("Err: %v\n", err)
 
 		// centerX := img.Bounds().Min.X + img.Bounds().Max.X/2
 		// centerY := img.Bounds().Min.Y + img.Bounds().Max.Y/2
 
 		// cropByCenterAndSpoke(img.Bounds(), centerX, centerY, 3, 1, 20000)
-		crop := lib.CropByCenterAndSpoke(img.Bounds(), image.Pt(centerX, centerY), ratioX, ratioY, spoke)
+		crop := lib.CropByCenterAndSpoke(img.Bounds(), centerX, centerY, ratioX, ratioY, spoke)
 
-		fmt.Println(crop.Min.X, crop.Min.Y, crop.Max.X, crop.Max.Y)
-		fmt.Println(crop)
+		// fmt.Println(crop.Min.X, crop.Min.Y, crop.Max.X, crop.Max.Y)
+		// fmt.Println(crop)
 
 		// todo can use sub image
 		cropped := image.NewRGBA(image.Rect(0, 0, crop.Max.X-crop.Min.X, crop.Max.Y-crop.Min.Y))
-
 		draw.Draw(cropped, cropped.Rect, img, image.Pt(0, 0), draw.Src)
 
-		//Global().Get("Uint8Array").New(len(bufff.Bytes()))
-		copied := js.CopyBytesToJS(dst, bufff.Bytes())
-		fmt.Println("COPIED", copied)
+		dst := js.Global().Get("Uint8Array").New(len(cropped.Pix))
+		_ = js.CopyBytesToJS(dst, cropped.Pix)
 
 		return map[string]interface{}{
-			"ORIGINAL": nil,
-			"1x1":      nil,
-			"1x2":      nil,
-			"1x3":      nil,
-			"1x4":      nil,
-			"2x1":      nil,
-			"2x3":      nil,
-			"3x1":      nil,
-			"3x2":      nil,
-			"3x4":      nil,
-			"4x1":      nil,
-			"4x2":      nil,
-			"4x3":      nil,
-			"9x16":     nil,
-			"16x9":     nil,
-			"test":     dst,
+			"success": dst,
 		}
 	})
 }
